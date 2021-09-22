ff146d3d816a8ccb1b0e78641a50a0a25692d011
zjregee <zjregee@gmail.com>
2021-09-22 11:44:01 +0800

    update

---
+++ 725.分隔链表.go
@@ -0,0 +1,37 @@
+/*
+ * @lc app=leetcode.cn id=725 lang=golang
+ *
+ * [725] 分隔链表
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func splitListToParts(head *ListNode, k int) []*ListNode {
+    n := 0
+    for node := head; node != nil; node = node.Next {
+        n++
+    }
+    quotient, remainder := n/k, n%k
+
+    parts := make([]*ListNode, k)
+    for i, curr := 0, head; i < k && curr != nil; i++ {
+        parts[i] = curr
+        partSize := quotient
+        if i < remainder {
+            partSize++
+        }
+        for j := 1; j < partSize; j++ {
+            curr = curr.Next
+        }
+        curr, curr.Next = curr.Next, nil
+    }
+    return parts
+}
+// @lc code=end
+
