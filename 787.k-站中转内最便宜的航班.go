d3e97c26ac7fc04457c53b1f2b8b5bb2fcbc4533
zjregee <zjregee@gmail.com>
2021-09-22 16:56:15 +0800

    update

---
+++ 787.k-站中转内最便宜的航班.go
@@ -0,0 +1,41 @@
+/*
+ * @lc app=leetcode.cn id=787 lang=golang
+ *
+ * [787] K 站中转内最便宜的航班
+ */
+
+// @lc code=start
+func findCheapestPrice(n int, flights [][]int, src int, dst int, k int) int {
+    const inf = 10000*101 + 1
+    f := make([][]int, k+2)
+    for i := range f {
+        f[i] = make([]int, n)
+        for j := range f[i] {
+            f[i][j] = inf
+        }
+    }
+    f[0][src] = 0
+    for t := 1; t <= k+1; t++ {
+        for _, flight := range flights {
+            j, i, cost := flight[0], flight[1], flight[2]
+            f[t][i] = min(f[t][i], f[t-1][j]+cost)
+        }
+    }
+    ans := inf
+    for t := 1; t <= k+1; t++ {
+        ans = min(ans, f[t][dst])
+    }
+    if ans == inf {
+        ans = -1
+    }
+    return ans
+}
+
+func min(a, b int) int {
+    if a < b {
+        return a
+    }
+    return b
+}
+// @lc code=end
+
