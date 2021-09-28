a2cdc6f40a16a9f20579e8a684b6b944d6d8d147
zjregee <zjregee@gmail.com>
2021-09-28 16:28:54 +0800

    update

---
+++ 437.路径总和-iii.go
@@ -0,0 +1,39 @@
+/*
+ * @lc app=leetcode.cn id=437 lang=golang
+ *
+ * [437] 路径总和 III
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func pathSum(root *TreeNode, targetSum int) int {
+	if root == nil {
+		return 0
+	}
+	res := rootSum(root, targetSum)
+	res += pathSum(root.Left, targetSum)
+	res += pathSum(root.Right, targetSum)
+	return res
+}
+
+func rootSum(root *TreeNode, targetSum int) (res int) {
+	if root == nil {
+		return
+	}
+	val := root.Val
+	if val == targetSum {
+		res++
+	}
+	res += rootSum(root.Left, targetSum-val)
+	res += rootSum(root.Right, targetSum-val)
+	return
+}
+// @lc code=end
+
