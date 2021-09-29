1bd6278e2f25551681560be631fb546b6cdebbb7
zjregee <zjregee@gmail.com>
2021-09-29 15:04:07 +0800

    update

--- 437.路径总和-iii.go
+++ 437.路径总和-iii.go
@@ -35,5 +35,25 @@ func rootSum(root *TreeNode, targetSum int) (res int) {
 	res += rootSum(root.Right, targetSum-val)
 	return
 }
+
+// 前缀和
+// func pathSum(root *TreeNode, targetSum int) (ans int) {
+// 	preSum := map[int64]int{0: 1}
+// 	var dfs func(*TreeNode, int64)
+// 	dfs = func(node *TreeNode, curr int64) {
+// 		if node == nil {
+// 			return
+// 		}
+// 		curr += int64(node.Val)
+// 		ans += preSum[curr-int64(targetSum)]
+// 		preSum[curr]++
+// 		dfs(node.Left, curr)
+// 		dfs(node.Right, curr)
+// 		preSum[curr]-- // 最重要的一行代码
+// 		return
+// 	}
+// 	dfs(root, 0)
+// 	return
+// }
 // @lc code=end
 
