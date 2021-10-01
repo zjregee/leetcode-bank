59e8e66d610c82b81e73f03512c1054a8cdb8e6d
zjregee <zjregee@gmail.com>
2021-10-01 14:51:12 +0800

    update

---
+++ 103.二叉树的锯齿形层序遍历.go
@@ -0,0 +1,44 @@
+/*
+ * @lc app=leetcode.cn id=103 lang=golang
+ *
+ * [103] 二叉树的锯齿形层序遍历
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func zigzagLevelOrder(root *TreeNode) (ans [][]int) {
+	if root == nil {
+		return
+	}
+	queue := []*TreeNode{root}
+	for level := 0; len(queue) > 0; level++ {
+		vals := []int{}
+		q := queue
+		queue = nil
+		for _, node := range q {
+			vals = append(vals, node.Val)
+			if node.Left != nil {
+				queue = append(queue, node.Left)
+			}
+			if node.Right != nil {
+				queue = append(queue, node.Right)
+			}
+		}
+		if level%2 == 1 {
+			for i, n := 0, len(vals); i < n / 2; i++ {
+				vals[i], vals[n-1-i] = vals[n-1-i], vals[i]
+			}
+		}
+		ans = append(ans, vals)
+	}
+	return
+}
+// @lc code=end
+
