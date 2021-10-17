6fa2219c79ce584d4e05aa81d4c01a6f99e7f4ca
zjregee <zjregee@gmail.com>
2021-10-17 10:52:18 +0800

    update

---
+++ 230.二叉搜索树中第k小的元素.go
@@ -0,0 +1,32 @@
+/*
+ * @lc app=leetcode.cn id=230 lang=golang
+ *
+ * [230] 二叉搜索树中第K小的元素
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func kthSmallest(root *TreeNode, k int) int {
+	stack := []*TreeNode{}
+	for {
+		for root != nil {
+			stack = append(stack, root)
+			root = root.Left
+		}
+		stack, root = stack[:len(stack)-1], stack[len(stack)-1]
+		k--
+		if k == 0 {
+			return root.Val
+		}
+		root = root.Right
+	}
+}
+// @lc code=end
+
