a29a06f4d46e63ee4469f43d3eff51e34a155c7a
zjregee <zjregee@gmail.com>
2021-10-21 10:47:21 +0800

    update

---
+++ 394.字符串解码.go
@@ -0,0 +1,48 @@
+/*
+ * @lc app=leetcode.cn id=394 lang=golang
+ *
+ * [394] 字符串解码
+ */
+
+// @lc code=start
+var (
+	src string
+	ptr int
+)
+
+func decodeString(s string) string {
+	src = s
+	ptr = 0
+	return getString()
+}
+
+func getString() string {
+	if ptr == len(src) || src[ptr] == ']' {
+		return ""
+	}
+	cur := src[ptr]
+	repTime := 1
+	ret := ""
+	if cur >= '0' && cur <= '9' {
+		repTime = getDigits()
+		ptr++
+		str := getString()
+		ptr++
+		ret = strings.Repeat(str, repTime)
+	} else if cur >= 'a' && cur <= 'z' || cur >= 'A' && cur <= 'Z' {
+		ret = string(cur)
+		ptr++
+	}
+	return ret + getString()
+}
+
+func getDigits() int {
+	ret := 0
+	for ; src[ptr] >= '0' && src[ptr] <= '9'; ptr++ {
+		ret = ret*10 + int(src[ptr]-'0')
+	}
+	return ret
+}
+
+// @lc code=end
+
