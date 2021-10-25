67996ce03d7d4d8cd99ec479df791d0dc4613d7c
zjregee <zjregee@gmail.com>
2021-10-25 08:58:54 +0800

    update

---
+++ 240.搜索二维矩阵-ii.go
@@ -0,0 +1,24 @@
+/*
+ * @lc app=leetcode.cn id=240 lang=golang
+ *
+ * [240] 搜索二维矩阵 II
+ */
+
+// @lc code=start
+func searchMatrix(matrix [][]int, target int) bool {
+    m, n := len(matrix), len(matrix[0])
+    x, y := 0, n-1
+    for x < m && y >= 0 {
+        if matrix[x][y] == target {
+            return true
+        }
+        if matrix[x][y] > target {
+            y--
+        } else {
+            x++
+        }
+    }
+    return false
+}
+// @lc code=end
+
